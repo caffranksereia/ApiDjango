72b7e8c06716a79c933db8fbf3046f7893e07f5f
ferafael <circuncisaofabio@icloud.com>
2021-04-26 16:37:15 -0300

    importado
    django,tamplate import loader
    .models import models
    from chefeCozinha.serializers import Cardapior serializer
    na criar classe Viewset.ModelsViewSet pegar queryset que recebe cardapio.objects.all() e o serlizer_class = cardapiorseliazer

--- ChefedeCozinha/polls/views.py
+++ ChefedeCozinha/polls/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from rest_framework import viewsets
+from .models import models
+
+from ChefedeCozinha.serializers import CardapioSerializer
+
+
+
+class (viewsets.ModelViewSet):
+    """
+        Api Endepoint that allows  users
+    """ 
+    queryset = Cardapio.objects.all()
+    serializer_class = CardapioSerializer
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello world!")
+
+def getAll(request):
+    listProduto = produto.objects('produto_id')
+    template = loader.get_template('polls/template/index.html')
+    context = {
+        'listProduto':listProduto,
+    }
+    return HttpResponse(template.render(context,request))
+
+def getId(request):
+    return HttpResponse("hello getId")
+
+def postProduto(request):
+    return HttpResponse("hello post")
+
+def editProduto(request):
+    return HttpResponse("hello edit")
+
+def delProduto(request):
+    return HttpResponse("hello delete")
