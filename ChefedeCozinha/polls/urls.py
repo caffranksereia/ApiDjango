20103c29cf26aff952a5cf5a296c9c0d46f4ba84
ferafael <circuncisaofabio@icloud.com>
2021-04-26 16:26:19 -0300

    URLS
    import routers do rest_framework e views do chefedeCozinha

--- ChefedeCozinha/polls/urls.py
+++ ChefedeCozinha/polls/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
-
+from django.urls import path,include
+from rest_framework import routers
+from ChefedeCozinha import views
 
 from . import views
 
+routers = routers.DefaultRouter()
+
+routers.register(r'getAllSet',views.getAllSet)
+
 urlpatterns = [
     path('',views.index,name='index'),
+    path('getAll',views.getAll,name="getAll"),
+    path('<int:produto_id>',views.getId,name='produto'),
+    path('postProduto/<int:produto_id>',views.postProduto,name="postProduto"),
+    path('editProduto/<int:produto_id',views.editProduto,name="editProduto"),
+    path('delProduto/<int:produto_id',views.delProduto,name="delProduto")
 ]
